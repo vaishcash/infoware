3ed7306f65b3810c3c3cef0ebcdf07ea47e9cde5
vaishkash <137786788+omAtDV@users.noreply.github.com>
2025-01-30 00:38:24 +0530

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import FormBuilder from "./components/FormBuilder";
 import FormPreviewPage from "./pages/FormPreviewPage";
 import Login from "./pages/Login";
+import { useEffect } from "react";
 
+function RouteComponent()
+{   const navigate = useNavigate();
+  useEffect(() => {
+    
+    navigate("/login");
+  }, [navigate]);
+  return (
+    <>
+
+    </>)
+}
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Routes>
           
+          <Route path="/" element={<RouteComponent />} />
           <Route path="/login" element={<Login />} />
           <Route path="/form-builder" element={<FormBuilder />} />
           <Route path="/preview" element={<FormPreviewPage />} />
